b58795b9c8d6b9b2fde546123c353de5a40b1a3a
meetvyas416@gmail.com <meetvyas416@gmail.com>
2023-11-18 01:14:02 +0530

    done

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Navigate to the application directory
-cd /home/ubuntu/weather-app
+cd /home/ubuntu/React-Ecommerce
 
 # Check if the Node.js application is running using pm2
 if pm2 id your-app-name > /dev/null 2>&1; then
